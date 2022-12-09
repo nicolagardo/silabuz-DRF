447cc08a29621d33bce4e732384573a85c5196a0
nicolagardo <nicolagardo@gmail.com>
2022-12-09 16:06:12 -0300

    aula03t2

--- myapi/myapi/settings.py
+++ myapi/myapi/settings.py
@@ -33,7 +33,8 @@ LOLCAL_APPS = [
     "todos"
 ]
 THIRD_APPS = [
-    "rest_framework"
+    "rest_framework",
+    "corsheaders"
 ]
 
 DJANGO_APPS = [
@@ -52,11 +53,20 @@ INSTALLED_APPS = DJANGO_APPS + LOLCAL_APPS + THIRD_APPS
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+# CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://myfrontend.com",
+    "http://localhost:4200",
+    "http://localhost:8000",
+    "http://127.0.0.1:5500",
 ]
 
 ROOT_URLCONF = "myapi.urls"
--- myapi/todos/serializer.py
+++ myapi/todos/serializer.py
@@ -20,3 +20,26 @@ class TodoSerializer(serializers.ModelSerializer):
        'updated_at', 
        'deleted_at',
        )
+
+
+class TestTodoSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length = 100)
+    body = serializers.CharField()
+
+    # def validate_simbol(self, value):
+    #     if "$" not in value:
+    #         return value
+    #     raise serializers.ValidationError("Error, el título no puede tenr el símbolo $")
+
+
+
+    def validate_title(self, value):
+        if "$" in value:
+            raise serializers.ValidationError("Error, el título no puede tenr el símbolo $")
+        return value
+
+    def validate_body(self, value):
+        if "$" in value:
+            raise serializers.ValidationError("Error, el título no puede tenr el símbolo $")
+        return value
