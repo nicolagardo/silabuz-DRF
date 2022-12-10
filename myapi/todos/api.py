a9f762ff59c25203b77704ca3ab537f6b82eceb6
nicolagardo <nicolagardo@gmail.com>
2022-12-10 20:02:02 -0300

    taller view jwt

--- myapi/todos/api.py
+++ myapi/todos/api.py
@@ -1,8 +1,46 @@
+
+
+from rest_framework import viewsets, permissions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 from .models import Todo
-from rest_framework import viewsets, permissions
 from .serializer import TodoSerializer, TestTodoSerializer
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
-    permission_classes = [permissions.AllowAny]
-    serializer_class = TestTodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = TodoSerializer
+
+
+class DeleteAllTodo(APIView):
+    def delete(self, request):
+        Todo.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class GetAllTodo(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request):
+        todos = Todo.objects.all()
+        seriralizer = TodoSerializer(todos, many= True)
+        return Response(seriralizer.data, status=status.HTTP_200_OK)
+
+class GetOneTodo(APIView):
+    def get(self, request, id):
+        todo = Todo.objects.filter(id= id).first()
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+class UpadateTodo(APIView):
+    def get(self, request, id):
+        todo = Todo.objects.filter(id=id).first()
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
+    def update(self, request):
+        todo = Todo.objects.filter(id=id).first()
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            # todo = serializer
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- myapi/todos/urls.py
+++ myapi/todos/urls.py
@@ -1,8 +1,31 @@
+from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 from rest_framework import routers
-from .api import TodoViewSet
+
+from .api import (
+    TodoViewSet,
+    DeleteAllTodo,
+    GetAllTodo,
+    GetOneTodo,
+    UpadateTodo
+)
 
 router = routers.DefaultRouter()
 
 router.register('/todos', TodoViewSet, 'todos')
 
-urlpatterns = router.urls
+
+urlpatterns = [
+    path('/todos/delete_all/', DeleteAllTodo.as_view(), name= 'delete_todo_all'),
+    path('/todos/get_all/', GetAllTodo.as_view(), name= 'get_todo_all'),
+    path('/todos/get/<id>', GetOneTodo.as_view(), name= 'get_todo'),
+    path('/todos/update/<id>', UpadateTodo.as_view(), name= 'upadtae_todo'),
+    path('/token', TokenObtainPairView.as_view(), name= 'token'),
+    path('/token/refresh/', TokenRefreshView.as_view(), name= 'refresh_token'),
+]
+
+urlpatterns += router.urls
